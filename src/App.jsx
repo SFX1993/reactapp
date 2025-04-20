e925d0dadf05989316ef53c2b37098f41ca1963c
John Gonsalves <thomasgonsalves12345@gmail.com>
2025-04-20 17:46:16 -0500

    working on the semantics of the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Search from "./components/search";
+const API_BASE_URL = "https://api.themoviedb.org/3";
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState(initialState, "");
+  const [searchTerm, setSearchTerm] = useState("");
+  useEffect(() => {}, []);
   return (
     <main>
       <div className="pattern">
         <div className="wrapper">
           <header>
-            <img
-              src="./hero.png
-            "
-              alt="Hero Banner"
-            />
+            <img src="./hero.png" alt="Hero Banner" />
             <h1>
               Find <span className="text-gradient">Movies</span> You'll Enjoy
               Without the Hassle
             </h1>
           </header>
-          <Search />
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </div>
       </div>
     </main>
