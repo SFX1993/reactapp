f611d743a33591bd09abc8dfbe129686a912997d
John Gonsalves <thomasgonsalves12345@gmail.com>
2025-04-21 19:51:19 -0500

    final day of building app

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+
 const MovieCard = ({
   movie: { title, vote_average, poster_path, release_date, original_language },
 }) => {
@@ -10,18 +11,22 @@ const MovieCard = ({
             ? `https://image.tmdb.org/t/p/w500/${poster_path}`
             : "/no-movie.png"
         }
-        alt="{title}"
+        alt={title}
       />
+
       <div className="mt-4">
         <h3>{title}</h3>
+
         <div className="content">
           <div className="rating">
             <img src="star.svg" alt="Star Icon" />
-            <p>{vote_average ? vote_average.tofixed(1) : "N/A"}</p>
+            <p>{vote_average ? vote_average.toFixed(1) : "N/A"}</p>
           </div>
-          <span>●</span>
+
+          <span>•</span>
           <p className="lang">{original_language}</p>
-          <span>●</span>
+
+          <span>•</span>
           <p className="year">
             {release_date ? release_date.split("-")[0] : "N/A"}
           </p>
